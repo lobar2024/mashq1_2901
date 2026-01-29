8a13135a2ee223a96831bf0d57770d745329d9ab
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-01-29 14:56:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#1
+x = 'hello world'
+yangi = x.title()
+print(yangi)
+
+x = '@pyton_1102@gmail.com'
+print(x.startswith('@'))
+
+result = lambda x: x[::-1].lower() == x[::-1].lower()
+print(result('RaDar'))
